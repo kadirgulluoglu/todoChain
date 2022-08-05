4b85fc53d1d2434150219d53d708fcc21860d8a2
kadirgulluoglu <62450370+kadirgulluoglu@users.noreply.github.com>
2022-08-05 11:49:43 +0300

    create flutter app with solidity

---
+++ android/app/src/main/kotlin/com/example/to_do_list_blockchain_dapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.to_do_list_blockchain_dapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TodoList Dapp',
+      home: TodoList(),
+    );
+  }
+}
+
+class TodoList extends StatelessWidget {
+  const TodoList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TodoList'),
+      ),
+    );
+  }
+}

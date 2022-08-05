0f80e6c0617d627148f7231e77d291105b9450d2
kadirgulluoglu <62450370+kadirgulluoglu@users.noreply.github.com>
2022-08-05 23:36:09 +0300

    HomePage responsive UI design done

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,49 +1,113 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 import 'package:to_do_list_blockchain_dapp/model/todolist_model.dart';
+import 'package:to_do_list_blockchain_dapp/screens/task_screen.dart';
+
+import '../theme/colors.dart';
 
 class TodoList extends StatelessWidget {
-  const TodoList({Key? key}) : super(key: key);
+  final TextEditingController _editingController = TextEditingController();
+
+  TodoList({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     var listModel = Provider.of<TodoListModel>(context);
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('TodoList'),
+      backgroundColor: primary,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+              isScrollControlled: true,
+              context: context,
+              builder: (context) => SingleChildScrollView(
+                  child: Container(
+                      padding: EdgeInsets.only(
+                          bottom: MediaQuery.of(context).viewInsets.bottom),
+                      child: AddTaskScreen())));
+        },
+        backgroundColor: Colors.indigo[400],
+        child: const Icon(Icons.add),
       ),
       body: listModel.isLoading
-          ? const Center(
-              child: CircularProgressIndicator(),
-            )
-          : Column(
-              children: [
-                Expanded(
-                  flex: 4,
-                  child: ListView.builder(
-                      itemCount: 10,
-                      itemBuilder: (context, index) =>
-                          const ListTile(title: Text("To do"))),
-                ),
-                Expanded(
-                    flex: 1,
-                    child: Row(
-                      children: [
-                        const Expanded(
-                          flex: 5,
-                          child: TextField(),
-                        ),
-                        Expanded(
-                          flex: 1,
-                          child: MaterialButton(
-                            onPressed: () {},
-                            child: const Text('Ekle'),
+          ? buildLoadingIndicator()
+          : SafeArea(
+              child: Column(
+                children: [
+                  buildIconAndName(),
+                  buildCountTaskText(listModel),
+                  Expanded(
+                    flex: 30,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: white,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(16))),
+                        child: ListView.builder(
+                          itemCount: listModel.taskCount,
+                          itemBuilder: (context, index) => ListTile(
+                            title: Text(
+                                listModel.todos[index].taskName.toString()),
                           ),
                         ),
-                      ],
-                    )),
-              ],
+                      ),
+                    ),
+                  ),
+                  buildSpacer(),
+                ],
+              ),
             ),
+    );
+  }
+
+  Spacer buildSpacer() => const Spacer(flex: 1);
+
+  Center buildLoadingIndicator() {
+    return const Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  Expanded buildCountTaskText(TodoListModel listModel) {
+    return Expanded(
+      flex: 1,
+      child: Text(
+        listModel.taskCount.toString() + " Tasks",
+        style: const TextStyle(color: white, fontSize: 18),
+      ),
+    );
+  }
+
+  Expanded buildIconAndName() {
+    return Expanded(
+      flex: 3,
+      child: Row(
+        children: const [
+          Expanded(
+            flex: 2,
+            child: Icon(
+              Icons.playlist_add_check,
+              size: 40,
+              color: white,
+            ),
+          ),
+          Spacer(flex: 1),
+          Expanded(
+            flex: 8,
+            child: Text(
+              "Todo Chain",
+              style: TextStyle(
+                  fontSize: 40, color: white, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }

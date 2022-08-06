2f6556345bec856a1ac59c7905bcd01ac1b361fd
kadirgulluoglu <62450370+kadirgulluoglu@users.noreply.github.com>
2022-08-06 22:54:09 +0300

    Change color

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
-const Color primary = Color(0xFF4DB6AC);
+const Color primary = Color(0xFF3A6263);
+const Color action = Color(0XFFC0CAD0);
 const Color black = Color(0xFF000000);
 const Color white = Color(0xFFFFFFFF);

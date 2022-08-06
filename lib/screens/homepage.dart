13a521a710e786bb786fc94cbe5bc15f00ddf097
kadirgulluoglu <62450370+kadirgulluoglu@users.noreply.github.com>
2022-08-06 22:56:00 +0300

    Extract methods

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,33 +1,22 @@
 import 'dart:ui';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 import 'package:to_do_list_blockchain_dapp/model/todolist_model.dart';
-import 'package:to_do_list_blockchain_dapp/screens/task_screen.dart';
 
 import '../theme/colors.dart';
+import 'bottom_sheet.dart';
 
 class TodoList extends StatelessWidget {
-  final TextEditingController _editingController = TextEditingController();
-
-  TodoList({Key? key}) : super(key: key);
+  const TodoList({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     var listModel = Provider.of<TodoListModel>(context);
-    Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: primary,
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          buildBottomSheet(context);
-        },
-        backgroundColor: Colors.indigo[400],
-        child: const Icon(Icons.add),
-      ),
+      floatingActionButton: buildFloatingActionButton(context),
       body: listModel.isLoading
           ? buildLoadingIndicator()
           : SafeArea(
@@ -43,6 +32,16 @@ class TodoList extends StatelessWidget {
     );
   }
 
+  FloatingActionButton buildFloatingActionButton(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () {
+        buildBottomSheet(context);
+      },
+      backgroundColor: action,
+      child: const Icon(Icons.add),
+    );
+  }
+
   Expanded buildTodoListView(TodoListModel listModel) {
     return Expanded(
       flex: 30,
@@ -52,43 +51,43 @@ class TodoList extends StatelessWidget {
           decoration: const BoxDecoration(
               color: white,
               borderRadius: BorderRadius.all(Radius.circular(16))),
-          child: ListView.builder(
-            itemCount: listModel.taskCount,
-            itemBuilder: (context, index) => ListTile(
-              title: Text(
-                listModel.todos[index].taskName.toString(),
-                style: TextStyle(
-                    fontSize: 20,
-                    decoration: listModel.todos[index].isCompleted
-                        ? TextDecoration.lineThrough
-                        : null),
-              ),
-              onTap: () {
-                listModel.toggleComplete(listModel.todos[index].id);
-              },
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Future<dynamic> buildBottomSheet(BuildContext context) {
-    return showModalBottomSheet(
-      backgroundColor: Colors.transparent,
-      isScrollControlled: true,
-      context: context,
-      builder: (context) => ClipRRect(
-        borderRadius: BorderRadius.circular(16),
-        child: BackdropFilter(
-          filter: ImageFilter.blur(sigmaY: 16, sigmaX: 16),
-          child: SingleChildScrollView(
-            child: Container(
-                decoration: BoxDecoration(color: Colors.white.withOpacity(0.4)),
-                padding: EdgeInsets.only(
-                  bottom: MediaQuery.of(context).viewInsets.bottom,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ListView.builder(
+              itemCount: listModel.taskCount,
+              itemBuilder: (context, index) => GestureDetector(
+                onHorizontalDragStart: (dragStartDetails) {
+                  listModel.toggleComplete(listModel.todos[index].id);
+                },
+                child: ListTile(
+                  title: Row(
+                    children: [
+                      Text("● ",
+                          style: TextStyle(
+                            fontSize: 25,
+                            color: listModel.todos[index].isCompleted
+                                ? action
+                                : black,
+                          )),
+                      Text(
+                        listModel.todos[index].taskName.toString(),
+                        style: TextStyle(
+                            fontSize: 25,
+                            color: listModel.todos[index].isCompleted
+                                ? action
+                                : black,
+                            decoration: listModel.todos[index].isCompleted
+                                ? TextDecoration.lineThrough
+                                : null),
+                      ),
+                    ],
+                  ),
+                  onTap: () {
+                    buildBottomSheet(context, task: listModel.todos[index]);
+                  },
                 ),
-                child: AddTaskScreen()),
+              ),
+            ),
           ),
         ),
       ),

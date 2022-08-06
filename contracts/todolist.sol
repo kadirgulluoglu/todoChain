e3f93b09371c359fcb8c4aaa3c406ee03a9f88df
kadirgulluoglu <62450370+kadirgulluoglu@users.noreply.github.com>
2022-08-06 18:08:08 +0300

    add update and delete task

--- contracts/todolist.sol
+++ contracts/todolist.sol
@@ -1,25 +1,52 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.22 <0.9.0;
 
-contract TodoList {
-    uint256 public taskCount;
+contract TodoContract {
+    uint256 public taskCount = 0;
 
     struct Task {
+        uint256 index;
         string taskName;
-        bool isCompleted;
+        bool isComplete;
     }
 
     mapping(uint256 => Task) public todos;
-
     event TaskCreated(string task, uint256 taskNumber);
-
-    constructor() public {
-        todos[0] = Task("Deneme", true);
-        taskCount = 1;
-    }
+    event TaskUpdated(string task, uint256 taskId);
+    event TaskIsCompleteToggle(string task, uint256 taskId, bool isComplete);
+    event TaskDeleted(uint256 taskNumber);
 
     function createTask(string memory _taskName) public {
-        todos[taskCount++] = Task(_taskName, false);
+        todos[taskCount++] = Task(taskCount, _taskName, false);
 
+        // emit event
         emit TaskCreated(_taskName, taskCount - 1);
+    }
+
+    function updateTask(uint256 _taskId, string memory _taskName) public {
+        Task memory currTask = todos[_taskId];
+        todos[_taskId] = Task(_taskId, _taskName, currTask.isComplete);
+
+        // emit event
+        emit TaskUpdated(_taskName, _taskId);
+    }
+
+    function toggleComplete(uint256 _taskId) public {
+        Task memory currTask = todos[_taskId];
+        todos[_taskId] = Task(_taskId, currTask.taskName, !currTask.isComplete);
+
+        // emit event
+        emit TaskIsCompleteToggle(
+            currTask.taskName,
+            _taskId,
+            !currTask.isComplete
+        );
+    }
+
+    function deleteTask(uint256 _taskId) public {
+        delete todos[_taskId];
+
+        // emit event
+        emit TaskDeleted(_taskId);
     }
 }
